39914d4ca9d03ef5a7881b05946e9d23d739e770
IZANA <fenomenal11>
2024-05-28 11:34:26 -0500

    template laravel

---
+++ template_practica/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title','dashboard')
+@section('content-header','Dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    </div>
+@endsection
